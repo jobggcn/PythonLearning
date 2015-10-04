cbcc64d63ffccdccdf2a18f53a65470cdb66b620
jobggcn <oliverbaumbach@163.com>
2015-10-04 13:02:46 +0800

    Chapter 4 Exercise 1

---
+++ automatetheboringstuff/commaprint.py
@@ -0,0 +1,13 @@
+# Comma Code Chapter 4 Practice Project 1 
+
+test_list = ['hello','world','how','is','it','going']
+
+
+def comma_print(my_list):
+    _output = "" 
+    for element in my_list[0:-1]:
+        _output += element + ", "
+    _output += my_list[-1]
+    return _output
+
+print(comma_print(test_list))

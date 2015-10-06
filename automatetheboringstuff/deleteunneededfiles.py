2467d7bd9666bb2069355673e2f84527e31bf202
jobggcn <oliverbaumbach@163.com>
2015-10-06 15:54:48 +0800

    Finished script
    Put in a try catch to filter out the silly broken file link bug, and added a function to print things out using pretty formatting and the humanize library. Also added the command line stuff, so this script should work as intended now.

--- automatetheboringstuff/deleteunneededfiles.py
+++ automatetheboringstuff/deleteunneededfiles.py
@@ -1,7 +1,8 @@
 #Exercise 2 Chapter 9 
 #Deleting Unneeded Files 
 
-import os, sys, time 
+import os, sys, time, humanize
+
 
 def list_files(folder):
     files = []
@@ -14,15 +15,34 @@ def sort_files_by_size_and_recency(file_names):
     file_info = []
     for dir_path,file_name in file_names:
         file_path = os.path.join(dir_path,file_name)
-        last_opened = os.stat(file_path).st_atime
-        elapsed_time = time.time() - float(last_opened)
-        file_size = os.stat(file_path).st_size 
-        score = file_size * elapsed_time
-        file_info.append((dir_path, file_name, elapsed_time, file_size, score))
+        try:
+            file_size = os.stat(file_path).st_size 
+            last_access_time = os.stat(file_path).st_atime
+            time_since_access = time.time() - last_access_time
+            file_info.append(\
+    (dir_path,file_name,file_size,time_since_access))
+        except IOError:
+            print('{} not found'.format(file_path))
+    file_info.sort(key = lambda x: x[2],reverse = True)
+    return file_info 
 
-    file_info.sort(key=lambda score: score[4])
-    for result in file_info[0:100]:
-       print(result[1])
+def print_file_info(files, number = 100):
+    for current_file in files[0:number]:
+        dir_path,file_name,file_size,last_access_time = current_file
+        full_path = os.path.join(dir_path,file_name)
+        file_size = humanize.naturalsize(file_size)
+        last_access_time = humanize.naturaltime(last_access_time)
+        print('Path:{}\n Filesize:{}, Last accessed:{}'.format(full_path,file_size,last_access_time))
 
-files = list_files(r"./")
-sort_files_by_size_and_recency(files)
+if len(sys.argv) == 2: 
+    script, path = sys.argv
+    if os.path.isdir(path):
+        files = list_files(path)
+        file_info = sort_files_by_size_and_recencz(files)
+        print_file_info(file_info)
+    else:
+        print("invalid folder path") 
+else:
+    print("deleteunneededfiles.py <path>") 
+
+  
